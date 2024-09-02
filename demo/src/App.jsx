07e631e2d304adb13ce3622642af8101feb30500
Vlaimir Korba <krissky1986@gmail.com>
2024-09-02 16:34:19 +0300

    #119_Using_Complex_Object_As_State_In_useReducer

--- demo/src/App.jsx
+++ demo/src/App.jsx
@@ -1,18 +1,30 @@
 import { useState, useReducer } from "react";
 
 function App() {
-    const [state, dispatch] = useReducer(reducer, { count: 0 });
+    const [state, dispatch] = useReducer(reducer, { count: 0, incrementBy: 1 });
     function reducer(state, action) {
         if (action.type === "increment") {
-            return { ...state, count: state.count + action.payload }; //state + action.payload;
+            return { ...state, count: state.count + state.incrementBy }; //state + action.payload;
         }
         if (action.type === "decrement") {
-            return { ...state, count: state.count - action.payload }; //state - action.payload;
+            return { ...state, count: state.count - state.incrementBy }; //state - action.payload;
+        }
+        if (action.type === "setIncrement") {
+            return { ...state, incrementBy: action.payload }; //state - action.payload;
         }
     }
     return (
         <div>
             {state.count}
+            <input
+                value={state.incrementBy}
+                onChange={(e) =>
+                    dispatch({
+                        type: "setIncrement",
+                        payload: Number(e.target.value),
+                    })
+                }
+            />
             <button onClick={() => dispatch({ type: "increment", payload: 1 })}>
                 Increment count
             </button>
