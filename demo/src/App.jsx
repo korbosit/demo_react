3793e499e90dacc32eb1445c32f470464f773ab2
Vlaimir Korba <krissky1986@gmail.com>
2024-09-02 16:14:44 +0300

    #118_Using_Object_As_State_In_useReducer

--- demo/src/App.jsx
+++ demo/src/App.jsx
@@ -1,10 +1,24 @@
-import Modal from "./components/Modal";
-import Parent from "./components/Parent";
+import { useState, useReducer } from "react";
 
 function App() {
+    const [state, dispatch] = useReducer(reducer, { count: 0 });
+    function reducer(state, action) {
+        if (action.type === "increment") {
+            return { ...state, count: state.count + action.payload }; //state + action.payload;
+        }
+        if (action.type === "decrement") {
+            return { ...state, count: state.count - action.payload }; //state - action.payload;
+        }
+    }
     return (
-        <div className="App">
-            <Modal />
+        <div>
+            {state.count}
+            <button onClick={() => dispatch({ type: "increment", payload: 1 })}>
+                Increment count
+            </button>
+            <button onClick={() => dispatch({ type: "decrement", payload: 1 })}>
+                Decrement count
+            </button>
         </div>
     );
 }
